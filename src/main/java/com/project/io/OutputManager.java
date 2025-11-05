8f0f1821ed5ea4991ccedbad5082e1cc077a606f
Salavat-dev0 <akmanaevsalavat@mail.ru>
2025-11-05 11:38:15 +0500

    Refactor IO (Input/Output) to use List

--- src/main/java/com/project/io/OutputManager.java
+++ src/main/java/com/project/io/OutputManager.java
@@ -5,18 +5,19 @@ import com.project.model.Car;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class OutputManager {
 
     /**
-     * Сохраняет массив автомобилей в текстовый файл.
+     * Сохраняет список автомобилей в файл.
      * Каждая строка файла содержит данные об одном автомобиле в формате:
      * Модель,Мощность,Год
      *
-     * @param cars массив объектов Car для сохранения
+     * @param cars список объектов Car для сохранения
      * @param path путь к файлу для сохранения данных
      */
-    public static void saveToFile(Car[] cars, String path) {
+    public static void saveToFile(List<Car> cars, String path) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(path))) {
             for (Car car : cars) {
                 writer.printf("%s,%d,%d\n",
