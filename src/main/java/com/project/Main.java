a0b297cf00dfe8801755e403317fcf8774851679
Eugene-godzilla <musihineugene@gmail.com>
2025-11-11 22:07:25 +0300

    final commit

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,10 +1,8 @@
 package com.project;
 
-import com.project.model.Car;
-import com.project.model.CarComparators;
+
 import com.project.mainapp.Application;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
